a4403f362a1b02e3c4997d310cf9e856f81ce170
Fabian Bell <fabian.bell@tum.de>
2020-12-01 07:40:52 +0000

    add first approach

---
+++ agent.py
@@ -0,0 +1,104 @@
+from transformers import T5Tokenizer, logging
+logging.set_verbosity_error()
+import re
+import torch
+
+from model import *
+
+class Agent:
+
+    def _get_tokenizer(self):
+        EOB = '<EOB>'
+        BELIEF_PREFIX = ' => Belief State : Movie Tickets { '
+        BELIEF_SUFFIX = ' } ' + EOB
+        KB_PREFIX = ' DB: '
+        EOKB = '<EOKB>'
+        tokenizer = T5Tokenizer.from_pretrained('t5-small',
+            additional_special_tokens=[EOB, BELIEF_PREFIX, BELIEF_SUFFIX, KB_PREFIX, EOKB, '{', '}', 'assistant:', 'user:', *[f'<extra_id_{i}>' for i in range(100)]])
+        return tokenizer
+
+    def __init__(self):
+        self.model = DialogueRestyler(inference=True)
+        self.model.load_state_dict(torch.load('model_DialogueRestyler.pt'))
+        self.tokenizer = self._get_tokenizer()
+        self.bs_pattern = re.compile(r'.*{\s\s(?P<data>.*)\s\s}.*')
+        self.queried = []
+
+    def _get_bs_dict(self, seq):
+        match = self.bs_pattern.match(seq)
+        if match is not None:
+            data = match.group('data')
+            data = [elem.split(' = ') for elem in data.split('; ')]
+            data = {k : v for k, v in data}
+            return data
+        return dict()
+
+    # name.movie = frozen; location = Koblenz; name.theater = KINOPOLIS Koblenz; date.showing = today
+    def db(self, data):
+        query = list(data.keys())
+        if 0 not in self.queried and {'name.movie', 'location', 'date.showing', 'time.showing', 'num.tickets'}.issubset(set(query)):
+            self.queried.append(0)
+            return ' DB  book_tickets { status = success } <EOKB>'
+        if 1 not in self.queried and {'name.movie', 'location'}.issubset(set(query)):
+            self.queried.append(1)
+            return ' DB:  find_theater { name.cinema_1 = KINOPOLIS Koblenz } <EOKB>'
+        if 2 not in self.queried and {'name.movie','location', 'name.theater', 'date.showing'}.issubset(set(query)):
+            self.queried.append(2)
+            return ' DB: find_showtimes { time.showing_1 = 10 pm ; time.showing_2 = 10 am } <EOKB>'
+        return ' DB:  <EOKB>'
+
+    def _get_beliefe_state(self, history):
+        input_ids = self.tokenizer(history, return_tensors='pt').input_ids
+        pred = self.model.generate(
+            torch.zeros(1, 1, dtype=torch.long),
+            torch.zeros(1, 1, dtype=torch.long),
+            input_ids,
+            torch.ones(input_ids.shape[0], input_ids.shape[1] + 1, dtype=torch.long),
+            torch.zeros(1, 512),
+            torch.zeros(1, 1, dtype=torch.long),
+            torch.zeros(1, 1, dtype=torch.long),
+            max_length=512,
+            modify_style=False
+        )
+        beliefe_state = self.tokenizer.decode(pred[0])
+        return beliefe_state
+    
+    def _get_response(self, intermediate, target, source):
+        input_ids = self.tokenizer(intermediate, return_tensors='pt').input_ids
+        target_ids, target_mask = self.tokenizer(target, return_tensors='pt').values()
+        source_ids, source_mask = self.tokenizer(source, return_tensors='pt').values()
+        pred = self.model.generate(
+            target_ids,
+            target_mask,
+            input_ids,
+            torch.ones(input_ids.shape[0], input_ids.shape[1] + 1, dtype=torch.long),
+            torch.tensor([[0.2, 0.4, 0.2, 0.4] + [0.]* 508]),
+            target_ids,
+            target_mask,
+            max_length=512
+        )
+        response = self.tokenizer.decode(pred[0])
+        return response
+
+    def __call__(self):
+        history = ''
+        target = ''
+        source = ''
+        while True:
+            user = ' user: ' + str(input('>>>'))
+            target += user
+            history += user
+            beliefe_state = self._get_beliefe_state(history)
+            print(beliefe_state)
+            bs_data = self._get_bs_dict(beliefe_state)
+            kb_data = self.db(bs_data)
+            print(kb_data)
+            intermediate = ''.join([history, beliefe_state, kb_data])
+            response = self._get_response(intermediate, target, source)
+            output = ' assistant: ' + response
+            source += output
+            history += output
+            print(f'>>>{response}')
+
+agent = Agent()
+agent()
---
+++ training.py
@@ -0,0 +1,100 @@
+import torch
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+import os
+
+from datasets import *
+from model import *
+
+class TrainingModel(pl.LightningModule):
+
+  def __init__(self, dataset_con, model_con, lr, batch_size, patience=2):
+    super(TrainingModel, self).__init__()
+    self.model = model_con()
+    self.dataset_con = dataset_con
+    self.batch_size = batch_size
+    self.lr = lr
+    self.patience = patience
+  
+  def forward(self, *inp):
+    out = self.model(*inp)
+    return out
+  
+  def get_dataloader(self, split, shuffle):
+    """
+    Creates the dataloader for the given split (['train', 'val', 'test'])
+    """
+    dataset = self.dataset_con(split) 
+    dataloader = DataLoader(dataset, batch_size=self.batch_size, 
+                            shuffle=shuffle, collate_fn=dataset.collate_batch, 
+                            num_workers=4)
+    return dataloader
+  
+  def load_pretrained(self):
+    self.model.load_pretrained()
+  
+  def train_dataloader(self):
+    return self.get_dataloader('train', True)
+
+  def val_dataloader(self):
+    return self.get_dataloader('val', False)
+  
+  def test_dataloader(self):
+    return self.get_dataloader('test', False)
+
+  def configure_optimizers(self):
+    optimizer = AdamW(self.parameters(), lr=self.lr)
+    scheduler = ReduceLROnPlateau(optimizer, patience=self.patience, verbose=True)
+    return {
+        'optimizer' : optimizer,
+        'scheduler' : scheduler,
+        'monitor'   : 'val_loss'
+    }
+
+  def base_step(self, batch):
+    context, context_mask, corrupted, corrupted_mask, target, prefix = batch
+    out = self(context, context_mask, corrupted, corrupted_mask, target, prefix)
+    loss = out.loss
+    return loss
+
+  def training_step(self, batch, batch_nb):
+    loss = self.base_step(batch)
+    self.log('train_loss', loss)
+    return loss
+  
+  def validation_step(self, batch, batch_nb):
+    loss = self.base_step(batch)
+    self.log('val_loss', loss)
+    return loss
+
+"""## Training"""
+
+#@title #Hyperparameters 
+def run_training():
+  name = 'DialogueRestyler' #@param {type: "string"}
+  lr = 1e-3 #@param
+  optimize_every = 5#@param
+  batch_size=12 #@param
+  patience=0 #@param
+  epochs = 100 #@param
+  check_val_every_n_epoch = 0.5 #@param
+  model_con = lambda: DialogueRestyler(apply_back_translation=True)
+  dataset_con = lambda split: StyleDialogueDataset(split, dim=512)
+  model = TrainingModel(dataset_con, model_con, lr=lr, 
+                      batch_size=batch_size, patience=patience)
+  if os.path.exists(f'model_{name}.pt'):
+    model.model.load_state_dict(torch.load(f'model_{name}.pt'))
+  trainer = pl.Trainer(
+      max_epochs=epochs,
+      check_val_every_n_epoch=check_val_every_n_epoch, 
+      gpus=[5, 6, 7],
+      accelerator='dp',
+      accumulate_grad_batches=optimize_every
+      )
+  trainer.fit(model)
+  torch.save(model.model.state_dict(), f'model_{name}.pt')
+
+#run_training()
